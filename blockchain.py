f6ddfda77cd8a6fba26b550ec1129e65846bd016
zcao5 <47010492+zcao5@users.noreply.github.com>
2019-06-21 15:33:37 -0400

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,116 @@
+import hashlib as hasher
+import datetime as date
+from time import time
+
+class Blockchain:
+    def __init__(self, data=0):
+        self.current_transactions = []
+        self.chain = []
+        self.nodes = set()
+        #the data we sharing
+        self.data = data
+        # Define the genesis block
+        self.new_block(previous_hash='1', proof=100)
+    def valid_chain(self, chain) -> bool:
+        """
+        Determine if a given blockchain is valid
+
+        :param chain: A blockchain
+        :return: True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+    def new_block(self, proof, previous_hash, voting_doc, voting_pat):
+        """
+        define new block here
+
+        :param proof: The proof given by the Proof of Work algorithm
+        :param previous_hash: Hash of previous Block
+        :return: New Block
+        """
+        # voting is the value that if the patients willing to share the code
+        if voting_doc or voting_pat ==0:
+            return Null
+        else:
+            block = {
+                'index': len(self.chain) + 1,
+                'timestamp': time(),
+                'transactions': self.current_transactions,
+                'proof': proof,
+                'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            }
+
+            # Reset the current list of transactions
+            self.current_transactions = []
+
+            self.chain.append(block)
+            return block
+
+    def new_transaction(self, sender: str, recipient: str, amount):
+        """
+        define new transaction
+
+        :param sender: Address of the Sender
+        :param recipient: Address of the Recipient
+        :param amount: Amount
+        :return: The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        define new SHA-256 hash value
+
+        :param block: Block
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof: int, proof: int) -> bool:
+        
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
